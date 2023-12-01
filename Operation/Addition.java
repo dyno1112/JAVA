e3c1185096483a9c355bef215cc180c2d9357ed7
dyno1112 <151719325+dyno1112@users.noreply.github.com>
2023-12-01 09:06:30 +0530

    Add files via upload

---
+++ Operation/Addition.java
@@ -0,0 +1,11 @@
+package Operation;
+
+public class Addition {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public float subtract(float x, float y) {
+        return x - y;
+    }
+}
---
+++ Operation/Maximum.java
@@ -0,0 +1,8 @@
+package Operation;
+
+public class Maximum {
+    public void max(int a, int b) {
+        System.out.println("Maximum of " + a + " and " + b + " is: " + Math.max(a, b));
+    }
+}
+
